91e259efeb60f4fa19cd91ba16cddf3e92069237
Dai Fujita <fujita.dai@gmail.com>
2013-06-18 22:02:58 +0900

    Gather ml operations into a procedure and tidy up

--- r/aoj-problem-clust-regression.r
+++ r/aoj-problem-clust-regression.r
@@ -3,11 +3,15 @@ library(lsa)
 
 # source("read-aoj-problem-data.r")
 
-writeClustersFromClasses <- function(classes, file) {
+writeMsgIfCan <- function(file=NULL, obj) {
+  if (!is.null(file)) msgpack.writeResult(file, msgpackR::pack(obj))
+}
+
+writeClustersFromClasses <- function(file, classes) {
   clusters <- lapply( split(1:length(probs), classes),
     function(cluster) { unlist(lapply(cluster, function(n) { prob.ids[n] })) })
   names(clusters) <- NULL  # remove names to pack as array
-  msgpack.writeResult(file, msgpackR::pack(clusters))
+  writeMsgIfCan(file, clusters)
 }
 
 doSkmeans <- function(file="clusters-50.msg", k=50, popsize=6, maxiter=50) {
@@ -19,7 +23,6 @@ doSkmeans <- function(file="clusters-50.msg", k=50, popsize=6, maxiter=50) {
 
   result
 }
-# doSkmeans("clusters-70.msg", k=70, popsize=12, maxiter=60)
 
 doMclust <- function(file="clusters-mixed.msg", G=1:9, modelNames=NULL) {
   library(mclust)
@@ -30,7 +33,6 @@ doMclust <- function(file="clusters-mixed.msg", G=1:9, modelNames=NULL) {
 
   mclust.fit
 }
-# doMclust("clusters-mixed-eee50.msg", G=50, modelNames="EEE")
 
 doHclust <- function(file="clusters-h.msg", method="euclid") {
   dists <- switch(method,
@@ -43,11 +45,10 @@ doHclust <- function(file="clusters-h.msg", method="euclid") {
     function(n) {
       if (is.leaf(n)) prob.ids[attr(n, "label")]
     } )
-  msgpack.writeResult(file, msgpackR::pack(tree))
+  writeMsgIfCan(file, tree)
 
   hc
 }
-# doHclust("clusters-h-spherical.msg", method="spherical")
 
 
 getDifficulty <- function(easy.thres=10000, easy.score=60) {
@@ -59,9 +60,8 @@ getDifficulty <- function(easy.thres=10000, easy.score=60) {
   scores <- scores[solved.inds, ]  # take away problems not listed in feature data
   ind.in.mat <- match(scores[, 1], prob.ids)  # search index in the given data
 
-  # 難易度表だけじゃ回帰無理でした、Volume0の問題が難易度500くらいになってしまいます
-  # ID0000～0020とID1000の問題について自前で難易度を設定しました（全て20以上90以下）
-  # あと一定数以上に解かれてて難易度未定義な問題は難易度60ってことでお願いします
+  # 難易度表だけだとうまく回帰できなかったので、
+  # 一定数以上に解かれていて難易度未定義な問題は適当に低く難易度設定する
   easy.inds <- which(table(rows) >= easy.thres & !(1:length(probs) %in% ind.in.mat))
   ind.in.mat <- c(ind.in.mat, easy.inds)
   
@@ -72,77 +72,95 @@ getDifficulty <- function(easy.thres=10000, easy.score=60) {
        unsolved.prob.ids = unsolved.prob.ids)
 }
 
+getDifficultyWithAutoEasy <- function() getDifficulty(easy.thres=600, easy.score=60)
+
 doRegressionGlmnet <- function(file="scores-lasso.msg", alpha=1, s=0.01) {
   library(glmnet)
 
-  scores <- getDifficulty(600, 60)
+  scores <- getDifficultyWithAutoEasy()
 
   set.seed(1)  # おそらく不要
-  # 少なくともelastic-netとlassoは高次元と変数の相関に強くないので次元削減したのを使う
-  lnet.fit <- glmnet(as.matrix(scores$compressed.feats), scores$values, alpha=alpha)
-  # predict(lnet.fit, newx=as.matrix(compressed.feats[match(201:300, prob.ids), ]), s=0.1)
-  preds <- predict(lnet.fit, newx=as.matrix(compressed.feats), s=s)
-  data.frame(prob.ids, preds, 1:length(probs) %in% scores$ind.in.mat)
+  # Ridge以外は高次元と変数の相関に強くないので次元削減したのを使う
+  scored.feats <- if (alpha == 0) scores$weighted.feats else scores$compressed.feats
+  new.feats <- if (alpha == 0) weighted.feats else compressed.feats
+  lnet.fit <- glmnet(as.matrix(scored.feats), scores$values, alpha=alpha)
+  pred <- predict(lnet.fit, newx=as.matrix(new.feats), s=s)
+  data.frame(prob.ids, pred, 1:length(probs) %in% scores$ind.in.mat)
 
-  if (!is.null(file)) msgpack.writeResult(file, msgpackR::pack(list(problemIds=prob.ids, scores=as.numeric(preds))))
+  writeMsgIfCan(file, list(problemIds=prob.ids, scores=as.numeric(pred)))
 
-  list(lnet=lnet.fit, pred=preds)
+  list(lnet=lnet.fit, pred=pred)
 }
-# cv.glmnet(as.matrix(scored.feats), scores$values, alpha=0.5)
-# doRegressionGlmnet(file="scores-ridge.msg", alpha=0, s=21)  # これはweighted.featsを使う
-# doRegressionGlmnet(file="scores-lasso.msg", alpha=1, s=0.01)
-# lnets <- doRegressionGlmnet(file="scores-elastic.msg", alpha=0.5, s=0.35)
+# cv.glmnet(as.matrix(scored.feats), scores$values, alpha=0.5)  # 交差検定あんまり役に立たない…
 
-doRegressionRf <- function(file="scores-rf.msg", ntree=1000) {
+doRegressionRf <- function(file="scores-rf.msg", ntree=1000, mtry=790) {
   library(randomForest)
 
-  scores <- getDifficulty(600, 60)
+  scores <- getDifficultyWithAutoEasy()
 
   set.seed(1)
-  rf <- randomForest(scores$weighted.feats, scores$values, ntree=ntree)
+  rf <- randomForest(as.matrix(scores$weighted.feats), scores$values, ntree=ntree)
   plot(rf)
   pred <- predict(rf, as.matrix(weighted.feats))
   print( data.frame(prob.ids, pred, 1:length(probs) %in% scores$ind.in.mat) )
 
-  if (!is.null(file)) msgpack.writeResult(file, msgpackR::pack(list(problemIds=prob.ids, scores=as.numeric(pred))))
+  writeMsgIfCan(file, list(problemIds=prob.ids, scores=as.numeric(pred)))
 
   list(rf=rf, pred=pred)
 }
 
-doRegressionNnet <- function(file="scores-nnet.msg") {
+doRegressionNnet <- function(file="scores-nnet.msg", size=3, maxit=100, decay=0) {
   library(nnet)
 
-  scores <- getDifficulty(600, 60)
+  scores <- getDifficultyWithAutoEasy()
 
   set.seed(1)
   nnet.fit <- nnet(as.matrix(scores$weighted.feats), scores$values / max(scores$values),
-    size=3, MaxNWts=20000, maxit=1000, decay=0.001)
+    size=size, MaxNWts=30000, maxit=maxit, decay=decay)
   pred <- predict(nnet.fit, weighted.feats) * max(scores$values)
   print( data.frame(prob.ids, pred, 1:length(probs) %in% scores$ind.in.mat) )
 
-  if (!is.null(file)) msgpack.writeResult(file, msgpackR::pack(list(problemIds=prob.ids, scores=as.numeric(pred))))
+  writeMsgIfCan(file, list(problemIds=prob.ids, scores=as.numeric(pred)))
 
   list(nnet=nnet.fit, pred=pred)
 }
 
-doRegressionEpsSvr <- function(file="scores-epssvr.msg", kernel="vanilladot", C=2, epsilon=0.08) {
+doRegressionEpsSvr <- function(file="scores-epssvr.msg", kernel="vanilladot", C=1, epsilon=0.1) {
   library(kernlab)
 
-  scores <- getDifficulty(600, 60)
+  scores <- getDifficultyWithAutoEasy()
 
-  epssvm <- ksvm(scores$compressed.feats, rscores$values / max(scores$values), type="eps-svr", kernel=kernel, C=C, epsilon=epsilon)
-  pred <- predict(epssvm, compressed.feats) * max(scores$values)
+  epssvm <- ksvm(as.matrix(scores$weighted.feats), scores$values / max(scores$values),
+    type="eps-svr", kernel=kernel, C=C, epsilon=epsilon)
+  pred <- predict(epssvm, as.matrix(weighted.feats)) * max(scores$values)
   print( data.frame(prob.ids, pred, 1:length(probs) %in% scores$ind.in.mat) )
 
-  if (!is.null(file)) msgpack.writeResult(file, msgpackR::pack(list(problemIds=prob.ids, scores=as.numeric(pred))))
+  writeMsgIfCan(file, list(problemIds=prob.ids, scores=as.numeric(pred)))
 
   list(svr=epssvm, pred=pred)
 }
-# doRegressionEpsSvr(file="scores-epssvr-vanilladot.msg", kernel="vanilladot", C=2, epsilon=0.08)
-# doRegressionEpsSvr(file="scores-epssvr-rbfdot.msg", kernel="rbfdot", C=8, epsilon=0.05)
+
 
 genTrainingScoreMsg <- function(file="training-scores.msg") {
-  scores <- getDifficulty(600, 60)
-  msgpack.writeResult(file, msgpackR::pack(list(problemIds=prob.ids[scores$ind.in.mat], scores=scores$values)))
+  scores <- getDifficultyWithAutoEasy()
+  writeMsgIfCan(file, list(problemIds=prob.ids[scores$ind.in.mat], scores=scores$values))
 }
 # 今更だが別にmsgpackじゃなくてjsonでよかった
+
+# うまい具合にできたやつだけまとめて吐く
+genAllMsgFiles <- function() {
+  doSkmeans("clusters-sk-50.msg", k=50, popsize=12, maxiter=70)
+  doSkmeans("clusters-sk-70.msg", k=70, popsize=12, maxiter=60)
+  doMclust("clusters-mixed-eee50.msg", G=50, modelNames="EEE")
+  doHclust("clusters-h-spherical.msg", method="spherical")
+
+  doRegressionGlmnet(file="scores-ridge.msg", alpha=0, s=21)
+  doRegressionGlmnet(file="scores-lasso.msg", alpha=1, s=0.01)
+  doRegressionGlmnet(file="scores-elastic.msg", alpha=0.5, s=0.35)
+  doRegressionRf(file="scores-rf.msg", ntree=1000)
+  doRegressionNnet(file="scores-nnet.msg", size=4, maxit=1000, decay=0.001)
+  doRegressionEpsSvr(file="scores-epssvr-vanilladot.msg", kernel="vanilladot", C=1, epsilon=0.04)
+  doRegressionEpsSvr(file="scores-epssvr-rbfdot.msg", kernel="rbfdot", C=3, epsilon=0.03)
+
+  genTrainingScoreMsg()
+}
